928090960a19832f2dad6428bf0ceded1ee1a572
avinash12345-dot <67866640+avinash12345-dot@users.noreply.github.com>
2024-12-20 14:22:10 +0530

    Create output-gcp.tf

---
+++ output-gcp.tf
@@ -0,0 +1,22 @@
+##output for vpc##
+output "name" {
+  description = "Name of the vpc"
+  value       = google_compute_network.vpc_network.name
+}
+
+output "id" {
+  description = "id of the vpc"
+  value       = google_compute_network.vpc_network.id
+}
+
+output "self_link" {
+  description = "self_link"
+  value       = google_compute_network.vpc_network.self_link
+}
+
+output "subnet_name" {
+  description = "Map of Subnet Name"
+  value = tomap({
+    for k, f in google_compute_subnetwork.vpc_subnetwork : k => f.name
+  })
+}
